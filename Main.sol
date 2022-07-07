5bda89d5158b6846f5812ee543dd842fdb4a2b61
thepirateboy <a_rafi@live.com>
2022-07-07 12:42:44 +1000

    Added potential governance mecanism.

--- Main.sol
+++ Main.sol
@@ -5,9 +5,12 @@ pragma solidity ^0.8.4;
 import "./security/Pausable.sol";
 import "./access/Ownable.sol";
 import "./ERC20.sol";
+import "./extensions/ERC20Permit.sol";
+import "./extensions/ERC20Votes.sol";
 
-contract REST is ERC20, Pausable, Ownable {
-    constructor() ERC20("Real Estate", "REST") {
+
+contract RealEstate is ERC20, Pausable, Ownable, ERC20Permit, ERC20Votes {
+    constructor() ERC20("Real Estate", "REST") ERC20Permit("Real Estate") {
         _mint(msg.sender, 100000000 * 10 ** decimals());
     }
 
@@ -25,5 +28,28 @@ contract REST is ERC20, Pausable, Ownable {
         override
     {
         super._beforeTokenTransfer(from, to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
     }
 }
